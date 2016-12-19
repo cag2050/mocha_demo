125a482af8f2b504eb645f37bbc0bc37d9280f15
cag2050 <cag2050@163.com>
2016-12-19 20:06:04 +0800

    m

---
+++ src/js/add.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chenanguo on 16/12/19.
+ */
+function add(x, y) {
+    return x + y;
+}
+
+module.exports = add;
---
+++ src/js/add.test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by chenanguo on 16/12/19.
+ */
+var add = require('./add.js');
+var expect = require('chai').expect;
+
+describe('加法函数的测试', function () {
+    it('1加1应该等于2', function () {
+        expect(add(1, 1)).to.be.equals(2);
+    })
+});
